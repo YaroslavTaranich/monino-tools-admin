d3a9cc7b1457d7dfbf4e747d0e4a40024f750f5f
Yaroslav Taranich <YaroslavTaranich@yandex.ru>
2025-06-04 21:40:14 +0300

    add titles to forms, fix fields order

--- src/components/categoryForm.tsx
+++ src/components/categoryForm.tsx
@@ -1,6 +1,6 @@
 import React, {FC, PropsWithChildren} from "react";
 import {ICategory} from "../services/categoryService";
-import {Form, Input} from "antd";
+import {Divider, Form, Input, Typography} from "antd";
 import TextArea from "antd/es/input/TextArea";
 
 interface ICategoryForm extends PropsWithChildren {
@@ -14,57 +14,63 @@ const CategoryForm: FC<ICategoryForm> = ({
                                              children,
                                          }) => {
     return (
-        <Form layout="vertical" onFinish={onFinish}>
-            <Form.Item
-                label="Навание Eng"
-                name="name"
-                initialValue={defaultValues?.name}
-                required
-            >
-                <Input/>
-            </Form.Item>
-            <Form.Item
-                label="Навание Ru"
-                name="label"
-                initialValue={defaultValues?.label}
-                required
-            >
-                <Input/>
-            </Form.Item>
-            <Form.Item
-                label="Заголовок на странице"
-                name="title"
-                initialValue={defaultValues?.title}
-                required
-            >
-                <Input/>
-            </Form.Item>
-            <Form.Item
-                label="html title"
-                name="html_title"
-                initialValue={defaultValues?.html_title}
-                required
-            >
-                <Input/>
-            </Form.Item>
-            <Form.Item
-                label="Описание"
-                name="description"
-                initialValue={defaultValues?.description}
-                required
-            >
-                <TextArea rows={5}/>
-            </Form.Item>
-            <Form.Item
-                label="Описание html"
-                name="html_description"
-                initialValue={defaultValues?.html_description}
-                required
-            >
-                <TextArea rows={5}/>
-            </Form.Item>
-            <Form.Item>{children}</Form.Item>
-        </Form>
+      <Form layout="vertical" onFinish={onFinish}>
+        <Divider />
+        <Typography.Title level={3}>Заголовки</Typography.Title>
+        <Form.Item
+          label="Навание Eng"
+          name="name"
+          initialValue={defaultValues?.name}
+          required
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Навание Ru"
+          name="label"
+          initialValue={defaultValues?.label}
+          required
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Заголовок на странице"
+          name="title"
+          initialValue={defaultValues?.title}
+          required
+        >
+          <Input />
+        </Form.Item>
+        <Divider />
+        <Typography.Title level={3}>Описание</Typography.Title>
+        <Form.Item
+          label="Описание"
+          name="description"
+          initialValue={defaultValues?.description}
+          required
+        >
+          <TextArea rows={5} />
+        </Form.Item>
+        <Divider />
+        <Typography.Title level={3}>SEO блок</Typography.Title>
+        <Form.Item
+          label="SEO title для страницы"
+          name="html_title"
+          initialValue={defaultValues?.html_title}
+          required
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="SEO description для страницы"
+          name="html_description"
+          initialValue={defaultValues?.html_description}
+          required
+        >
+          <TextArea rows={5} />
+        </Form.Item>
+        <Form.Item>{children}</Form.Item>
+      </Form>
     );
 };
 
